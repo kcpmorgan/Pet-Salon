496c3c7412178caf6f34d5a866797617662e7e83
Kim Morgan <101612445+kcpmorgan@users.noreply.github.com>
2022-04-18 16:56:14 -0700

    updates

--- scripts/register.js
+++ scripts/register.js
@@ -1,8 +1,10 @@
 let petSalon = {
     name:"The Fashion Pet",
     address:{
-        street:"Ave University",
+        street:"University Ave",
         number:"787",
+        city:"Utopia",
+        State:"CA",
         zip:"23456"
     },
     hours:{
@@ -11,15 +13,57 @@ let petSalon = {
     },
     pets:[
         {
-            //obj1
+            name:"Scooby",
+            age: 50,
+            breed:"Dane",
+            gender:"Male",
+            service:"Grooming",
+            ownersname: "Shaggy",
+            contactphone:"555-555-555"
+
         },
         {
-            //obj2
+            name:"Scrappy",
+            age:40,
+            breed:"Mixed",
+            gender:"Male",
+            service:"Grooming",
+            ownersname:"Shaggy",
+            contactphone:"555-555-555"
         },
         {
-            //obj3
+            name:"Zest",
+            age:15,
+            breed:"Chihuahua",
+            gender:"Male",
+            service:"Spa",
+            ownersname:"Brenda",
+            contactphone:"555-555-555"
+        },
+        {
+            name:"Leo",
+            age:12,
+            breed:"Poodle",
+            gender:"Male",
+            service:"Grooming",
+            ownersname:"Aria",
+            contactphone:"555-555-555"
         }
     ]
 }
 
 console.log(petSalon);
+function displaySalonInfo(){
+    let tmp="";
+    let div=document.getElementById("pets");
+    div.innerHTML=tmp
+}
+console.log(pets)
+function displayPetsNames(){
+    let tmp="";
+    let div=document.getElementById("petSalon");
+    for(let i=0;i<petSalon.length;i++){
+        tmp+=`<li>${petSalon[i].pets}</li>`;
+    }
+    div.innerHTML=tmp;
+}
